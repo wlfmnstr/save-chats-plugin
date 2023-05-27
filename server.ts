4d7ffe98008dda401bae838e99723f6b38faa0d9
Casey <casey@fidgetlabs.co>
2023-05-27 00:44:37 -0500

    Initial api stubbed

---
+++ server.ts
@@ -0,0 +1,31 @@
+// a simple express node ts api server
+
+import express from 'express';
+import { Request, Response } from 'express';
+import { createServer } from 'http';
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+// Post method with query parameters for chatId
+app.post('/saveChat', (req: Request, res: Response) => {
+    const chat = req.body
+    res.send('Chat saved!');
+});
+
+app.get('/getChat/:', (req: Request, res: Response) => {
+    // Get the chat from the database
+    // Return the chat
+    res.send('Chat retrieved!');
+});
+
+const server = createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
